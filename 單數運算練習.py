a8d8b01e65102e51c1d10a34981cd3b512819016
fhopecc <fhopecc@gmail.com>
2022-06-26 16:30:36 +0800

    加上固定左數功能

--- 單數運算練習.py
+++ 單數運算練習.py
@@ -1,15 +1,22 @@
 from pathlib import Path
 wdir = Path(__file__).parent
 wordexe = 'C:\Program Files\Microsoft Office\Office14\WINWORD.EXE'
-def 單數運算評量(運算='X', 列印=False):
+
+def 單數運算評量(運算='X', 列印=False, 固定左數:int=0):
     運算名稱 = {'X':'乘法', '+':'加法'}[運算]
-    table = [(x,y) for x in range(1,10) for y in range(1,10)]
+    if 固定左數:
+        table = [(int(固定左數), y) for y in range(1,10)]
+        table.extend(table*8)
+    else:
+        table = [(x,y) for x in range(1,10) for y in range(1,10)]
     from random import shuffle
     shuffle(table)
     from docx import Document
     f = wdir / '模版.docx'
     doc = Document(f)
-    doc.paragraphs[0].text = f"九九{運算名稱}評量"
+    title = f"九九{運算名稱}評量"
+    title += f'，固定左數為{固定左數}' if 固定左數>0 else ''
+    doc.paragraphs[0].text = title
     doc.paragraphs[0].style = doc.styles['Heading 1']
     tab = doc.add_table(rows=9, cols=9)
     for i, c in enumerate(table):
@@ -34,8 +41,8 @@ def 單數運算評量(運算='X', 列印=False):
         if 運算=='X':
             c.text = (f'{n1}X{n2}=({n1*n2})')
         elif 運算=='+': 
+            # breakpoint()
             c.text = (f'{n1}+{n2}=({n1+n2})')
- 
     fn = wdir / f'九九{運算名稱}評量.docx'
     doc.save(fn)
     from os import system
@@ -49,8 +56,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("operator", help="運算類型")
     parser.add_argument("--print", action='store_true', help="列印")
+    parser.add_argument("--fix", help="固定左數")
     args = parser.parse_args()
-    if args.print:
-        單數運算評量(args.operator, 列印=True)
-    else:
-        單數運算評量(args.operator)
+    單數運算評量(args.operator, 列印=args.print, 固定左數=int(args.fix))
